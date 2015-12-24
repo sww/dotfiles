b0209c7e5e8ae4fcc1d89037b0306c6617e2da8d
sww <sean@decrypted.org>
2015-12-24 14:28:37 -0800

    Set warnings and errors to highlight the whole line.

--- emacs/.emacs.d/modes/flycheck-extras.el
+++ emacs/.emacs.d/modes/flycheck-extras.el
@@ -3,4 +3,7 @@
 (set-face-attribute 'flycheck-error nil :foreground "OrangeRed" :underline "Red")
 (set-face-attribute 'flycheck-warning nil :foreground nil :underline "lightpink2")
 
+;; Highlight the whole line on warning or error.
+(setq flycheck-highlighting-mode 'lines)
+
 (setq flycheck-display-errors-delay 0.15)
