d14fe50dc10f129ea8e14aaba2205c48c32d5e5b
sww <sean@decrypted.org>
2016-02-12 17:25:35 -0800

    Added the rename-this-buffer-and-file function.

--- emacs/.emacs.d/lisp/functions.el
+++ emacs/.emacs.d/lisp/functions.el
@@ -3,4 +3,22 @@
   "Create tags file."
   (interactive "DDirectory: ")
   (shell-command
-   (format "ctags -e -R %s" (directory-file-name dir-name))))
+   (format "ctags --exclude=.git --exclude=venv --exclude=node_modules -e -R %s" (directory-file-name dir-name))))
+
+;; http://rejeep.github.io/emacs/elisp/2010/03/26/rename-file-and-buffer-in-emacs.html.
+(defun rename-this-buffer-and-file ()
+  "Renames current buffer and file it is visiting."
+  (interactive)
+  (let ((name (buffer-name))
+        (filename (buffer-file-name)))
+    (if (not (and filename (file-exists-p filename)))
+        (error "Buffer '%s' is not visiting a file!" name)
+      (let ((new-name (read-file-name "New name: " filename)))
+        (cond ((get-buffer new-name)
+               (error "A buffer named '%s' already exists!" new-name))
+              (t
+               (rename-file filename new-name 1)
+               (rename-buffer new-name)
+               (set-visited-file-name new-name)
+               (set-buffer-modified-p nil)
+               (message "File '%s' successfully renamed to '%s'" name (file-name-nondirectory new-name))))))))
