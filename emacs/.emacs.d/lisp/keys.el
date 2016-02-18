bcffdd120fa823fd01c4ea98743b39873a664ea7
sww <sean@decrypted.org>
2016-02-18 11:52:51 -0800

    Moved swap-windows to functions.el.

--- emacs/.emacs.d/lisp/keys.el
+++ emacs/.emacs.d/lisp/keys.el
@@ -33,21 +33,6 @@
   (indent-region (point-min) (point-max) nil)
   (untabify (point-min) (point-max)))
 
-;; From https://sites.google.com/site/steveyegge2/my-dot-emacs-file.
-(defun swap-windows ()
- "If you have 2 windows, it swaps them." (interactive) (cond ((not (= (count-windows) 2)) (message "You need exactly 2 windows to do this."))
- (t
- (let* ((w1 (first (window-list)))
-	 (w2 (second (window-list)))
-	 (b1 (window-buffer w1))
-	 (b2 (window-buffer w2))
-	 (s1 (window-start w1))
-	 (s2 (window-start w2)))
- (set-window-buffer w1 b2)
- (set-window-buffer w2 b1)
- (set-window-start w1 s2)
- (set-window-start w2 s1)))))
-
 ;; http://emacsredux.com/blog/2013/03/30/go-back-to-previous-window/
 ;; Go backwards with C-x O when there are more than two windows split.
 (global-set-key (kbd "C-x O") (lambda ()
