2ae121880d2993aa6170359c75a35b8f76d2504a
sww <sean@decrypted.org>
2016-04-22 17:34:15 -0700

    Set the max helm window height.

--- emacs/.emacs.d/modes/helm-extras.el
+++ emacs/.emacs.d/modes/helm-extras.el
@@ -1,2 +1,8 @@
 (require 'helm)
 (require 'helm-ls-git)
+
+;; Turn on autoresize-mode to be able to set the max height.
+(helm-autoresize-mode 1)
+
+;; Set the max helm height to 50% of the buffer.
+(setq helm-autoresize-max-height 50)
