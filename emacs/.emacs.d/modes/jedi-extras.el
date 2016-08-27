ec58a969063409fd6ba6d29f3fec74afea42c889
sww <sean@decrypted.org>
2016-08-26 17:38:23 -0700

    Increased the Jedi tooltip delay.

--- emacs/.emacs.d/modes/jedi-extras.el
+++ emacs/.emacs.d/modes/jedi-extras.el
@@ -1,4 +1,4 @@
 ;; For jedi auto complete for Python.
 
 (setq jedi:complete-on-dot t)
-(setq jedi:get-in-function-call-delay 150) ;; Delay in showing the tool tip.
+(setq jedi:get-in-function-call-delay 1500) ;; Delay in showing the tool tip.
