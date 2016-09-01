421de0bb835c072d11870571e3974175f5e8b264
sww <sean@decrypted.org>
2016-08-31 22:22:22 -0700

    Switched to goimports instead of gofmt.

--- emacs/.emacs.d/modes/go-extras.el
+++ emacs/.emacs.d/modes/go-extras.el
@@ -1,6 +1,7 @@
 ;; Additional settings for go-mode.
 
+(setq gofmt-command "goimports")
 (add-hook 'before-save-hook #'gofmt-before-save)
-(setq gofmt-args (list "-s"))
+(setq gofmt-args nil)
 
 (add-hook 'go-mode-hook 'flycheck-mode)
