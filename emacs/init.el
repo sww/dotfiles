69a25afbb4329651b9b18a48a1a740f4a0d67130
Sean Wang <sean@decrypted.org>
2018-02-18 11:26:16 -0800

    Added GOPATH retrieval from the shell.

--- emacs/init.el
+++ emacs/init.el
@@ -6,7 +6,8 @@
 
 ;; Set the path from $PATH.
 (when (memq window-system '(mac ns))
-  (exec-path-from-shell-initialize))
+  (exec-path-from-shell-initialize)
+  (exec-path-from-shell-copy-env "GOPATH")
 
 ;; Sets the dotfiles emacs dir.
 (defvar emacsdir
