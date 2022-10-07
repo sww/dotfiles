3f80eb1f042c6a99b9816f80a2f7e7b1936b82dd
Sean Wang <sean@decrypted.org>
2022-10-06 22:51:47 -0700

    Have the LSP server handle the buffer formatting and import sorting.

--- emacs/init.el
+++ emacs/init.el
@@ -271,8 +271,13 @@
 
 (use-package go-mode
   :init
-  (add-hook 'before-save-hook #'gofmt-before-save)
   (add-hook 'go-mode-hook 'which-function-mode)
+  ;; Set up before-save hooks to format buffer and add/delete imports.
+  ;; Make sure you don't have other gofmt/goimports hooks enabled.
+  (defun lsp-go-install-save-hooks ()
+    (add-hook 'before-save-hook #'lsp-format-buffer t t)
+    (add-hook 'before-save-hook #'lsp-organize-imports t t))
+  (add-hook 'go-mode-hook #'lsp-go-install-save-hooks)
   :config
   (setq gofmt-command "goimports")
   (setq gofmt-args nil)
