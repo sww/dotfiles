0d9cd9854447d7dfee7a585f17f75cad567ec79b
Sean Wang <sean@decrypted.org>
2024-02-28 13:04:40 -0800

    Removed the elpaca use package mode.

--- emacs/init.el
+++ emacs/init.el
@@ -42,9 +42,7 @@
 ;; Install use-package support
 (elpaca elpaca-use-package
   ;; Enable :elpaca use-package keyword.
-  (elpaca-use-package-mode)
-  ;; Assume :elpaca t unless otherwise specified.
-  (setq elpaca-use-package-by-default t))
+  (elpaca-use-package-mode))
 
 ;; Block until current queue processed.
 (elpaca-wait)
